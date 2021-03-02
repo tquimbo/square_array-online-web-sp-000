ed621daf67afc916bb10c29993a09774d6fee6ca
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 14:36:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-square.array = []
-square_array.each do |numbers|
-square_array.push(numbers ** 2)
+array = []
+array.each do |numbers|
+array.push(numbers ** 2)
 end
-return square_array
+return array
 end
